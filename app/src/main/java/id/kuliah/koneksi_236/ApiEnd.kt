e84ed66b2b4df18ead5bb8b2305cfaf51148e8d1
refiaditya88 <refiaditya88@gmail.com>
2019-12-06 01:57:53 +0700

    Initial commit

---
+++ app/src/main/java/id/kuliah/koneksi_236/ApiEnd.kt
@@ -0,0 +1,11 @@
+package id.kuliah.koneksi_236
+
+class ApiEnd {
+    companion object{
+        private val SERVER = "http://192.168.43.1/universitaskotlin/"
+        val CREATE = SERVER+"create_fakultas.php"
+        val READ = SERVER+"read_fakultas.php"
+        val UPDATE = SERVER+"update_fakultas.php"
+        val DELETE = SERVER+"delete_fakultas.php"
+    }
+}
---
+++ app/src/main/java/id/kuliah/koneksi_236/Fakultas.kt
@@ -0,0 +1,3 @@
+package id.kuliah.koneksi_236
+
+data class Fakultas (val id_fakultas: String, val kode_fakultas:String?, val nama_fakultas:String?)
